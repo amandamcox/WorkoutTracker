3ebb6dd52e5193bf005c45280ebcba797ce21d0f
Amanda Cox <amandamcox@gmail.com>
2020-02-06 15:25:50 -0800

    Forgot the import, duh

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 usersRouter.get('/', async (req, res, next) => {
 	try {
