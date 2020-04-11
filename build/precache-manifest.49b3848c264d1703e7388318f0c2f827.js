5c6928662d5a81076610cc547881b20c6884bd27
Amanda Cox <amandamcox@gmail.com>
2020-04-10 18:47:42 -0700

    trying to fix static files

---
+++ build/precache-manifest.49b3848c264d1703e7388318f0c2f827.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4afafa82b8501da5689ea4ac7c97e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb98310b686e847ba75a",
+    "url": "/static/css/main.dc2be905.chunk.css"
+  },
+  {
+    "revision": "807e2f6552c6bfcf100f",
+    "url": "/static/js/2.7ec41fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ec41fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb98310b686e847ba75a",
+    "url": "/static/js/main.09b2d4bb.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "2025dbed3a1ebb89cf8d93276d71be0b",
+    "url": "/static/media/header-image.2025dbed.png"
+  }
+]);
